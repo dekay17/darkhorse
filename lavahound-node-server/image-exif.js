2b5fad07f73a28e80e23a151d058236837d5661c
Daniel Kelley <dan@kelleyland.com>
2015-09-10 09:11:11 -0400

    fixing image processing

--- lavahound-node-server/image-exif.js
+++ lavahound-node-server/image-exif.js
@@ -1,9 +1,12 @@
 var ExifImage = require('exif').ExifImage;
 var geolib = require('geolib');
+var path = require('path');
+var filename = process.argv[2];
 
+console.log('checking ', filename);
     
 try {
-    new ExifImage({ image : 'IMG_3295.JPG' }, function (error, exifData) {
+    new ExifImage({ image : filename }, function (error, exifData) {
         if (error)
             console.log('Error: '+error.message);
         else{
@@ -11,8 +14,8 @@ try {
         	var sexagesimalLng = exifData.gps.GPSLongitude[0] + "° " + exifData.gps.GPSLongitude[1] +"' " + exifData.gps.GPSLongitude[2] + "\" " + exifData.gps.GPSLongitudeRef;
         	var lat = geolib.sexagesimal2decimal(sexagesimalLat);
         	var lng = geolib.sexagesimal2decimal(sexagesimalLng);
-            console.log(lat + "," + lng); // Do something with your data! 
-            console.log("39.955846, -75.182942");
+            console.log(lat + "," + lng + "\t" + path.basename(filename)); 
+            // console.log("39.955846, -75.182942");
             console.log(exifData.gps); 
         }
     });
